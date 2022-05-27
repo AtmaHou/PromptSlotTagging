78050e8d42a7800d3d2c9a3bd49d349b650c8e0a
left111 <43339501+left111@users.noreply.github.com>
2022-05-27 20:06:02 +0800

    Update mit_mm.sh

--- scripts/mit/mit_mm.sh
+++ scripts/mit/mit_mm.sh
@@ -1,4 +1,4 @@
 #cd inverse
 source ~/init_conda.sh
 conda activate py36
-python train.py --do_test --log_output_path ./prompt_data/MIT_MM/prompt_MIT_MM/mir_mm.10_shot/log.txt --test_path ./prompt_data/MIT_MM/prompt_MIT_MM/ --test_file mit_mm.10_shot --gen_batch_size=50 --num_finetune_epochs 4 --dataset mit --pred_path ./pred/ --model_selection_path ./model_selection/
+python train.py --do_test --log_output_path ./pred/mir_mm.10_shot/log.txt --test_path ./prompt_data/MIT_MM/prompt_MIT_MM/ --test_file mit_mm.10_shot --gen_batch_size=50 --num_finetune_epochs 4 --dataset mit --pred_path ./pred/ --model_selection_path ./model_selection/
